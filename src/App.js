0b27b784377c85042053cbe897619c6629191926
enthoniNagel <enthoni.nagel.dev@gmail.com>
2024-09-03 20:07:58 -0300

    forme2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import React, { Component } from 'react';
+ 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: {
+        nome: '',
+        email: '',
+        senha: '',
+        sexo: ''
+      }
+    };
+    this.dadosForm = this.dadosForm.bind(this);
+  }
+ 
+ 
+ 
+  dadosForm(e){
+    let form = this.state.form;
+    form[e.target.name] = e.target.value;
+    this.setState({form: form});
+  }
+ 
+  render() {
+    return (
+      <div>
+        <h2>Login</h2>
+        Nome:
+        <input
+          type='text'
+          name='nome'
+          value={this.state.form.nome}
+          onChange={this.dadosForm}
+        /><br/>
+ 
+        Email:
+        <input
+          type='text'
+          name='email'
+          value={this.state.form.email}
+          onChange={this.dadosForm}
+        /><br/>
+ 
+        Senha:
+        <input
+          type='text'
+          name='senha'
+          value={this.state.form.senha}
+          onChange={this.dadosForm}
+        /><br/>
+ 
+        Sexo:
+        <select
+          name="sexo"
+          value={this.state.form.sexo}
+          onChange={this.dadosForm}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <option value="feminino">Feminino</option>
+          <option value="masculino">Masculino</option>
+        </select>
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
